05bdbd84b62e9cc3ee0addcc795cf49523c73e0c
root <root@DESKTOP-482253N.localdomain>
2021-09-30 20:25:15 +1000

    moving forward, but stuck

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # web-scraper-project
 Creating a web scraper for my Phase 3 Academy XI project
-
-Testing if commit works
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,31 +1,37 @@
-require 'open-uri'
 require 'nokogiri'
+require 'open-uri'
+require 'pry'
 
 require_relative './job.rb'
 
-class Scraper
-
-    url = "https://www.seek.com.au/junior-software-engineer-jobs/in-All-Australia/full-time?salaryrange=70000-999999&salarytype=annual"
-    html = open(url)
+class Scraper    
 
     def get_page
-        Nokogiri::HTML(html)
+        url = "https://www.seek.com.au/junior-software-engineer-jobs?salaryrange=70000-999999&salarytype=annual"
+        html = open(url)
+        doc = Nokogiri::HTML(html)
     end
 
     def get_jobs
-        self.get_page.css("div._1UfdD4q")
+        puts self.get_page.css("article._37iADb_ vwiJKjx")
+        self.get_page.css("article._37iADb_ vwiJKjx")
     end
 
     def make_jobs
-       self.get_courses.each do |post|
-            job = Job.new
-            job.job_title = post.css(div article span._3mgsa7- _2X_OUt_ _1WgeL1f _3VdCwhL _2Ryjovs h1 a.text)
-            job.company_name
-            job.location
-            job.statement
-            job.url
-        end 
+       self.get_jobs.each do |post|
+            print "iteration in progress"
+            # job = Job.new
+            # job.job_title = post.css("div article span._3mgsa7- _2X_OUt_ _1WgeL1f _3VdCwhL _2Ryjovs h1 a").text
+            # puts job.job_title
+            # job.company_name = post.css("div article span._3mgsa7- _15GBVuT _2Ryjovs span span._2cFajGc a").text
+            # job.location
+            # job.statement
+            # job.url
+        end
     end
 
 end
+
+Scraper.new.get_jobs
+
 
