fff77cb7545ba4bc98e94d5350f151c0cbc9130a
root <root@DESKTOP-482253N.localdomain>
2021-09-30 18:04:34 +1000

    added job class

---
+++ lib/job.rb
@@ -0,0 +1,19 @@
+class Job 
+
+    attr_accessor :job_title, :company_name, :location, :statement, :url
+
+    @@all = []
+
+    def initialize
+        @@all << self    
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.reset_all
+        @@all.clear
+    end
+
+end
