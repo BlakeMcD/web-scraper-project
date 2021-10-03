778e327aaec3da004c9817fbc855b65b83603078
root <root@DESKTOP-482253N.localdomain>
2021-10-03 19:15:16 +1100

    added company name to search info

--- lib/jsaajse-app/scraper.rb
+++ lib/jsaajse-app/scraper.rb
@@ -18,6 +18,7 @@ class JsaajseApp::Scraper
             job = JsaajseApp::Job.new
 
             job.job_title = post.css("h1").text.strip 
+            job.company_name = post.css("span._3mgsa7- a._17sHMz8").text.strip
             job.location = post.css("span._3mgsa7- strong._7ZnNccT a._17sHMz8").text.strip.gsub('Information & Communication Technology','') 
             job.statement = post.css("span._2OKR1ql").text.strip
             job.url = base_url+post.css("h1 a").first["href"] 
@@ -25,7 +26,7 @@ class JsaajseApp::Scraper
     end
 end
 
-# Scraper.new.make_jobs
+Scraper.new.make_jobs
 # p Job.all
 
 
