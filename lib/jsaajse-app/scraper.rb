29ca00f6fc62c911b58e6535e386afc44da9e8e0
root <root@DESKTOP-482253N.localdomain>
2021-10-03 23:16:13 +1100

    searches multiple seek pages

--- lib/jsaajse-app/scraper.rb
+++ lib/jsaajse-app/scraper.rb
@@ -1,7 +1,11 @@
 class JsaajseApp::Scraper    
 
+    attr_accessor :page_number
+
+    @@page_number = 1
+
     def get_page
-        url = "https://www.seek.com.au/junior-software-engineer-jobs?salaryrange=70000-999999&salarytype=annual"
+        url = "https://www.seek.com.au/junior-software-engineer-jobs?page=#{page_number}&salaryrange=70000-999999&salarytype=annual"
         html = open(url)
         doc = Nokogiri::HTML(html)
     end
@@ -22,6 +26,13 @@ class JsaajseApp::Scraper
             job.location = post.css("span._3mgsa7- strong._7ZnNccT a._17sHMz8").text.strip.gsub('Information & Communication Technology','') 
             job.statement = post.css("span._2OKR1ql").text.strip
             job.url = base_url+post.css("h1 a").first["href"] 
+        end
+    end
+
+    def make_jobs_for_multiple_pages
+        while @@page_number < 6 
+            make_jobs
+            @@page_number += 1
         end
     end
 end
