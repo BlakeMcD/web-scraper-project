aaec4965575f13291f98ce11060f09875ff23b39
root <root@DESKTOP-482253N.localdomain>
2021-10-03 17:59:23 +1100

    adding more functions

--- lib/jsaajse-app/job_cli.rb
+++ lib/jsaajse-app/job_cli.rb
@@ -5,7 +5,53 @@ class JsaajseApp::JobCLI
     end
 
     def start
-        puts "Welcome to my CLI App"    
+        system("clear")
+        puts "Welcome to JSAAJSE - An app that searches seek.com.au for Australian junior software engineer roles." 
+        puts ""
+        new_line()
+        divider()
+        new_line()
+        unique_locations() 
+    end
+
+    def list_jobs
+        JsaajseApp::Job.all.each.with_index(1) do |job, i|
+            puts "#{i}. #{job.job_title}"
+        end
+    end
+
+    def unique_locations
+        locations = []
+        JsaajseApp::Job.all.each do |job| 
+            unless locations.include?(job.location.strip)
+                locations << job.location
+            end  
+        end
+        locations << "Wherever"
+        display_unique_locations(locations)
+    end
+
+    def display_unique_locations(array)
+        array.sort.each.with_index(1) do |element, index|
+            puts "#{index}: #{element}"
+        end 
+    end
+
+    #Formatting functions 
+    def new_line
+        puts ""
+    end
+
+    def divider
+        puts "-------------------"
+    end
+
+    #prompt user
+    def prompt_location_selection
+        divider()
+        puts "select the city you would like to search for:"
+
+        input = gets.strip.to_i
     end
 
 end
